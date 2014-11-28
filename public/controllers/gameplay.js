6d0d781c5b6b570a074bd43e5424ac24212dc8ec
Dylan Harness <dharness@uwo.ca>
2014-11-27 21:58:02 -0500

    implemented basic gameplay images

--- public/controllers/gameplay.js
+++ public/controllers/gameplay.js
@@ -1,6 +1,38 @@
- console.log('in gameplay');
-
  // create the controller and inject Angular's $scope
  myapp.controller('gameplayController', function($scope) {
+
+     //set the image for the stain
+     $scope.imageName = 'test_img.png';
+
+     function testAnim(x) {
+         $('#animateTest').removeClass().addClass(x);
+         var wait = window.setTimeout(function() {
+                 $('#animateTest').removeClass()
+             },
+             1300
+         );
+     }
+
+     $(function() {
+         $pos = $('.sandbox').offset().top - 0;
+         console.log('works as you thought, dyl');
+
+         $(window).on('scroll', function() {
+             if ($(window).scrollTop() >= $pos) {
+                 $('.sandbox').addClass('fixed');
+             } else {
+                 $('.sandbox').removeClass('fixed');
+             }
+         });
+     });
+
+     $(document).ready(function() {
+         $('a[data-test]').click(function() {
+             console.log('data-set: [data-set]');
+             var anim = $(this).attr('data-test');
+             testAnim('bounceInDown');
+         });
+     });
+
 
  });
